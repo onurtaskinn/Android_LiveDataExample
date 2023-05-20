682e809d694615934f602f443e837e48ccf2c92f
onurtaskinn <onurtaskin180@gmail.com>
2023-05-20 05:17:50 +0200

    LiveData Transformations

--- app/src/main/java/es/uam/eps/dadm/myapplication/MainViewModel.kt
+++ app/src/main/java/es/uam/eps/dadm/myapplication/MainViewModel.kt
@@ -3,13 +3,24 @@ package es.uam.eps.dadm.myapplication
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
-class MainViewModel : ViewModel() {
+class MainViewModel: ViewModel() {
     private var liveData = MutableLiveData<Int>()
     val number: LiveData<Int>
         get() = liveData
+
+    val isEven: LiveData<Boolean> =
+        Transformations.switchMap(number, ::transform)
+
+    private fun transform(num: Int): MutableLiveData<Boolean> {
+        return if (num % 2 == 0)
+            MutableLiveData<Boolean>().apply { value = true }
+        else
+            MutableLiveData<Boolean>().apply { value = false }
+    }
 
     fun generateNumber() {
         liveData.value = Random.nextInt(100)
--- app/src/main/java/es/uam/eps/dadm/myapplication/SecondFragment.kt
+++ app/src/main/java/es/uam/eps/dadm/myapplication/SecondFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import es.uam.eps.dadm.myapplication.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
     private lateinit var binding: FragmentSecondBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +25,15 @@ class SecondFragment : Fragment() {
         )
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+
+        viewModel.isEven.observe(viewLifecycleOwner) {
+            binding.textView.text = if (it == true) "TRUE" else "FALSE"
+        }
     }
 }
